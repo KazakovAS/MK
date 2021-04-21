ae8d5ff9891796253053567c6a290b190da919bc
Alexey Kazakov <a.kazakov@betaonline.ru>
2021-04-21 09:28:11 +0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,37 @@ import { arenas } from "./const.js";
 import { player1 } from "./const.js";
 import { player2 } from "./const.js";
 
-import generateLogs from "./generateLogs.js";
-import createPlayer from "./createPlayer.js";
-import getDamage from "./getDamage.js";
+import createElement from "./createElement.js";
 
-generateLogs('start', player1, player2);
+function createPlayer(player) {
+  const playerBox = createElement('div', 'player' + player.player);
+  const progressBar  = createElement('div', 'progressbar');
+  const characterBox = createElement('div', 'character');
+  const life = createElement('div', 'life');
+  const name = createElement('div', 'name');
+  const img = createElement('img');
+
+  life.style.width = player.hp + '%';
+  img.src = player.img;
+  name.textContent = player.name;
+
+  playerBox.appendChild(progressBar);
+  playerBox.appendChild(characterBox);
+
+  progressBar.appendChild(life);
+  progressBar.appendChild(name);
+
+  characterBox.appendChild(img);
+
+  return playerBox;
+}
+
 arenas.appendChild(createPlayer(player1));
 arenas.appendChild(createPlayer(player2));
+
+import getDamage from "./getDamage.js";
+
+import generateLogs from "./generateLogs.js";
+generateLogs('start', player1, player2);
+
+
