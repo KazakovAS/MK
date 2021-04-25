7d94a0dacb974d054a204f8e0c860ccc0cbe8598
Alexey Kazakov <a.kazakov@betaonline.ru>
2021-04-25 17:34:05 +0300

    add class game

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,6 @@ import generateLogs from "./generateLogs.js";
 import getDamage from "./getDamage.js";
 import showResult from "./showResult.js";
 
-
 formFight.addEventListener('submit', function(e) {
   e.preventDefault();
 
@@ -14,15 +13,18 @@ formFight.addEventListener('submit', function(e) {
   showResult();
 })
 
+class Game {
+  start = async () => {
+    player1.createPlayer();
+    player2.createPlayer();
 
-function init() {
-  player1.createPlayer();
-  player2.createPlayer();
-
-  generateLogs('start', player1, player2);
+    generateLogs('start', player1, player2);
+  }
 }
 
-init();
+const game = new Game();
+game.start();
+
 
 
 
